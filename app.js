5c6c24268cf0611bfee301c6e0ae8102fc2fb3e6
Virtual Ark <acetrix01@gmail.com>
2025-07-10 15:41:16 +0300

    feat(openai): Add System prompt and OpenRouter API integration

--- app.js
+++ app.js
@@ -10,11 +10,33 @@ const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
 })
 
+const name = "Ian Hinga"
 
+let linkedinProfile = fs.readFileSync('Profile.pdf')
 
-let dataBuffer = fs.readFileSync('Profile.pdf')
-
-pdf(dataBuffer).then((data) => {
-    console.log(data.text);
+pdf(linkedinProfile).then((data) => {
+    //console.log(data.text);
 });
 
+let summaryBuffer = fs.readFileSync('summary.txt', 'utf8')
+
+//console.log(summaryBuffer)
+
+
+let system_prompt = `You are acting as ${name}. You are answering questions on {name}'s website,` +
+`particularly questions related to ${name}'s career, background, skills and experience. ` +
+`Your responsibility is to represent ${name} for interactions on the website as faithfully as possible.` +
+`You are given a summary of ${name}'s background and LinkedIn profile which you can use to answer questions.`
+`Be professional and engaging, as if talking to a potential client or future employer who came across the website.` +
+`If you don't know the answer, say so.`
+
+system_prompt += `\n\n## Summary:\n${summaryBuffer}\n\n## LinkedIn Profile:\n${linkedinProfile}\n\n`
+system_prompt += `With this context, please chat with the user, always staying in character as ${name}.`
+
+console.log(system_prompt)
+
+function chat(message, history) {
+    let messages = [{"role":"system", "content":system_prompt}] + history + [{"role":"user", "content":message}]
+    let response = openai.chat.completions.create(model="openai/gpt-4.1-mini", messages=messages);
+    return response.choices[0].message.content
+}
